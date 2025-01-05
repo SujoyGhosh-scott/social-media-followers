859c5ed487eeb14008c0d4ab7b79fde4ed385029
Sujoy Ghosh <ghoshsujoy336@gmail.com>
2025-01-05 10:59:26 +0530

    first commit

---
+++ scrapeLogic.js
@@ -0,0 +1,45 @@
+// import puppeteer from "puppeteer";
+const puppeteer = require("puppeteer");
+
+const scrapeLogic = async (req, res) => {
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch({
+    executablePath: "google-chrome-stable",
+  });
+
+  try {
+    const page = await browser.newPage();
+
+    // Navigate the page to a URL.
+    await page.goto("https://developer.chrome.com/");
+
+    // Set screen size.
+    await page.setViewport({ width: 1080, height: 1024 });
+
+    // Type into search box.
+    await page
+      .locator(".devsite-search-field")
+      .fill("automate beyond recorder");
+
+    // Wait and click on first result.
+    await page.locator(".devsite-result-item-link").click();
+
+    // Locate the full title with a unique string.
+    const textSelector = await page
+      .locator("text/Customize and automate")
+      .waitHandle();
+    const fullTitle = await textSelector?.evaluate((el) => el.textContent);
+
+    // Print the full title.
+    console.log("FULL TITLE: ", fullTitle);
+
+    res.status(200).send(fullTitle);
+  } catch (error) {
+    console.log("scrapping error: ", error);
+    res.status(500).send(error);
+  } finally {
+    await browser.close();
+  }
+};
+
+module.exports = { scrapeLogic };
