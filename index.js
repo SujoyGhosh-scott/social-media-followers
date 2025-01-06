81e3982ee238cd4403a6306ea5dd0e2e3df9e9e8
Sujoy Ghosh <ghoshsujoy336@gmail.com>
2025-01-06 21:28:53 +0530

    using yt apis

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
-const { scrapeLogic, countFollowers } = require("./scrapeLogic");
+const axios = require("axios");
+require("dotenv").config();
+
+// const { scrapeLogic, countFollowers } = require("./scrapeLogic");
 const app = express();
 app.use(express.json());
 
@@ -9,12 +12,37 @@ app.get("/", (req, res) => {
   res.status(200).send("Server is up and running");
 });
 
-app.post("/scrape", (req, res) => {
-  scrapeLogic(req, res);
-});
+// app.post("/scrape", (req, res) => {
+//   scrapeLogic(req, res);
+// });
 
-app.post("/count", (req, res) => {
-  countFollowers(req, res);
+// app.post("/count", (req, res) => {
+//   countFollowers(req, res);
+// });
+
+app.get("/subscribers/:channelId", async (req, res) => {
+  const date = new Date();
+  console.log("REQUEST TIME: ", date.toISOString());
+
+  if (!req.params.channelId)
+    return res.status(400).send({ success: false, error: "Invalid Params" });
+  console.log("CHANNEL: ", req.params.channelId);
+
+  try {
+    const response = await axios.get(
+      `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${req.params.channelId}&key=${process.env.API_KEY}`
+    );
+    const stats = response.data.items[0].statistics;
+    console.log("response: ", response.data);
+    // console.log(`SUBSCRIBERS: ${stats.subscriberCount}`);
+    res.status(200).send({ success: true, subscribers: stats.subscriberCount });
+  } catch (error) {
+    console.error(
+      "Error fetching subscribers:",
+      error.response ? error.response.data : error.message
+    );
+    res.status(500).send({ success: false, error });
+  }
 });
 
 app.listen(PORT, () => {
